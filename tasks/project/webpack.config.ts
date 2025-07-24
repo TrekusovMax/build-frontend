73935e8afdbde4978b60dbd8b0832bb6a6d906e9
Trekusov Max <maxtrekusovsite@gmail.com>
2025-07-24 16:58:16 +0300

    webpack done

--- tasks/project/webpack.config.ts
+++ tasks/project/webpack.config.ts
@@ -1,10 +1,58 @@
-import { Configuration } from "webpack";
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import { Configuration } from 'webpack'
+import path from 'node:path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 const config: Configuration = {
-  
-};
+  entry: './src/webpack.tsx',
+  mode: isProd ? 'production' : 'development',
+  target: ['web'],
+  output: {
+    path: path.resolve('./dist/webpack/'),
+    clean: true,
+    publicPath: '/webpack/',
+  },
+  devtool: false,
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    plugins: [
+      new TsconfigPathsPlugin({
+        extensions: ['.ts', '.tsx'],
+        configFile: path.resolve(import.meta.dirname, './tsconfig.json'),
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
 
-export default config;
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Webpack</title>
+</head>
+<body>
+  <div id="root"></div>
+</body>
+</html>`,
+    }),
+  ],
+}
+export default config
+
